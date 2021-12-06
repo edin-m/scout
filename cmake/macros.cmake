9b41e3835db41226a9a211930d9b593112ce1619
Edin Mujagic <edinmujagic2@gmail.com>
2021-12-06 01:48:25 +0100

    add imgui nanovg

--- cmake/macros.cmake
+++ cmake/macros.cmake
@@ -18,6 +18,15 @@
 #    target_link_libraries(${TARGET_NAME} SDL2_ttf)
 #endmacro()
 
+
+# todo: use "${TARGET_NAME}"
+# todo: use "${GIT_ROOT}/..."
+# todo: use target_include_directories
+
+macro(link_opengl TARGET_NAME)
+    target_link_libraries(${TARGET_NAME} opengl32.lib)
+endmacro()
+
 macro(link_glfw TARGET_NAME)
     target_include_directories(${TARGET_NAME} PUBLIC ${GIT_ROOT}/external/lib_bin/glfw-3.3.5-build/include)
     target_link_directories(${TARGET_NAME} PUBLIC ${GIT_ROOT}/external/lib_bin/glfw-3.3.5-build/lib/x86/Release)
@@ -30,17 +39,58 @@ macro(link_glad TARGET_NAME)
 endmacro()
 
 macro(link_nanovg TARGET_NAME)
-    target_include_directories(${TARGET_NAME} PUBLIC ${GIT_ROOT}/external/lib_bin/nanovg-build/include)
-    target_link_directories(${TARGET_NAME} PUBLIC ${GIT_ROOT}/external/lib_bin/nanovg-build/lib/x86/Release)
-    target_link_libraries(${TARGET_NAME} nanovg.lib)
+    include_directories(${GIT_ROOT}/external/lib_bin/nanovg-build/include)
+    target_link_libraries(${TARGET_NAME} ${GIT_ROOT}/external/lib_bin/nanovg-build/lib/x86/Release/nanovg.lib)
+endmacro()
+
+macro(link_imgui TARGET_NAME)
+    set(IMGUI_HOME ${GIT_ROOT}/external/lib_bin/imgui)
+    include_directories(${IMGUI_HOME} ${IMGUI_HOME}/backends)
+    file(GLOB IMGUI_CPP "${IMGUI_HOME}/imgui*.cpp")
+    target_sources(${TARGET_NAME} PRIVATE ${IMGUI_CPP})
+    target_sources(${TARGET_NAME} PRIVATE
+        "${IMGUI_HOME}/backends/imgui_impl_glfw.cpp"
+        "${IMGUI_HOME}/backends/imgui_impl_opengl3.cpp" )
 endmacro()
 
 macro(add_src_files TARGET_NAME)
     target_sources(${TARGET_NAME} PRIVATE ${SRC_SOURCES})
 endmacro()
 
-macro(link_libraries TARGET_NAME LIBS)
-    foreach(lib LIBS)
-        message("WIll link lib")
+macro(link_dependencies TARGET_NAME)
+    set(oneValueArgs TARGET_NAME)
+    set(multiValueArgs LIBS)
+    cmake_parse_arguments(ARG "${options}" "${oneValueArgs}"
+        "${multiValueArgs}" ${ARGN} )
+
+    foreach(lib ${ARG_LIBS})
+        # opengl
+        if (${lib} STREQUAL opengl)
+            link_opengl(${TARGET_NAME})
+        endif()
+
+        # glfw
+        if(${lib} STREQUAL glfw)
+            link_glfw(${TARGET_NAME})
+        endif()
+
+        # glad
+        if(${lib} STREQUAL glad)
+            link_glad(${TARGET_NAME})
+        endif()
+
+        # nanovg
+        if(${lib} STREQUAL nanovg)
+            link_nanovg(${TARGET_NAME})
+        endif()
+
+        # imgui
+        if(${lib} STREQUAL imgui)
+            link_imgui(${TARGET_NAME})
+        endif()
+
+        # nanogui
+#        elseif(${lib} STREQUAL imgui)
+#            link_imgui(${TARGET_NAME})
     endforeach()
 endmacro()
--- projects/examples/nanovg_example.cpp
+++ projects/examples/nanovg_example.cpp
@@ -6,6 +6,14 @@
 // GLFW
 #include <GLFW/glfw3.h>
 
+#include "nanovg/nanovg.h"
+#define NANOVG_GL3_IMPLEMENTATION
+#include "nanovg/nanovg_gl.h"
+
+//#define GLFW_INCLUDE_GLEXT
+//#include <GLFW/glfw3.h>
+
+
 
 // This example is taken from https://learnopengl.com/
 // https://learnopengl.com/code_viewer.php?code=getting-started/hellowindow2
@@ -21,17 +29,96 @@ void key_callback(GLFWwindow* window, int key, int scancode, int action, int mod
 // Window dimensions
 const GLuint WIDTH = 800, HEIGHT = 600;
 
+
+
+void drawGraph(NVGcontext* vg, float x, float y, float w, float h, float t)
+{
+    NVGpaint bg;
+    float samples[6];
+    float sx[6], sy[6];
+    float dx = w/5.0f;
+    int i;
+
+    samples[0] = (1+sinf(t*1.2345f+cosf(t*0.33457f)*0.44f))*0.5f;
+    samples[1] = (1+sinf(t*0.68363f+cosf(t*1.3f)*1.55f))*0.5f;
+    samples[2] = (1+sinf(t*1.1642f+cosf(t*0.33457)*1.24f))*0.5f;
+    samples[3] = (1+sinf(t*0.56345f+cosf(t*1.63f)*0.14f))*0.5f;
+    samples[4] = (1+sinf(t*1.6245f+cosf(t*0.254f)*0.3f))*0.5f;
+    samples[5] = (1+sinf(t*0.345f+cosf(t*0.03f)*0.6f))*0.5f;
+
+    for (i = 0; i < 6; i++) {
+        sx[i] = x+i*dx;
+        sy[i] = y+h*samples[i]*0.8f;
+    }
+
+    // Graph background
+    bg = nvgLinearGradient(vg, x,y,x,y+h, nvgRGBA(0,160,192,0), nvgRGBA(0,160,192,64));
+    nvgBeginPath(vg);
+    nvgMoveTo(vg, sx[0], sy[0]);
+    for (i = 1; i < 6; i++)
+        nvgBezierTo(vg, sx[i-1]+dx*0.5f,sy[i-1], sx[i]-dx*0.5f,sy[i], sx[i],sy[i]);
+    nvgLineTo(vg, x+w, y+h);
+    nvgLineTo(vg, x, y+h);
+    nvgFillPaint(vg, bg);
+    nvgFill(vg);
+
+    // Graph line
+    nvgBeginPath(vg);
+    nvgMoveTo(vg, sx[0], sy[0]+2);
+    for (i = 1; i < 6; i++)
+        nvgBezierTo(vg, sx[i-1]+dx*0.5f,sy[i-1]+2, sx[i]-dx*0.5f,sy[i]+2, sx[i],sy[i]+2);
+    nvgStrokeColor(vg, nvgRGBA(0,0,0,32));
+    nvgStrokeWidth(vg, 3.0f);
+    nvgStroke(vg);
+
+    nvgBeginPath(vg);
+    nvgMoveTo(vg, sx[0], sy[0]);
+    for (i = 1; i < 6; i++)
+        nvgBezierTo(vg, sx[i-1]+dx*0.5f,sy[i-1], sx[i]-dx*0.5f,sy[i], sx[i],sy[i]);
+    nvgStrokeColor(vg, nvgRGBA(0,160,192,255));
+    nvgStrokeWidth(vg, 3.0f);
+    nvgStroke(vg);
+
+    // Graph sample pos
+    for (i = 0; i < 6; i++) {
+        bg = nvgRadialGradient(vg, sx[i],sy[i]+2, 3.0f,8.0f, nvgRGBA(0,0,0,32), nvgRGBA(0,0,0,0));
+        nvgBeginPath(vg);
+        nvgRect(vg, sx[i]-10, sy[i]-10+2, 20,20);
+        nvgFillPaint(vg, bg);
+        nvgFill(vg);
+    }
+
+    nvgBeginPath(vg);
+    for (i = 0; i < 6; i++)
+        nvgCircle(vg, sx[i], sy[i], 4.0f);
+    nvgFillColor(vg, nvgRGBA(0,160,192,255));
+    nvgFill(vg);
+    nvgBeginPath(vg);
+    for (i = 0; i < 6; i++)
+        nvgCircle(vg, sx[i], sy[i], 2.0f);
+    nvgFillColor(vg, nvgRGBA(220,220,220,255));
+    nvgFill(vg);
+
+    nvgStrokeWidth(vg, 1.0f);
+}
+
+
 // The MAIN function, from here we start the application and run the game loop
 int main()
 {
+
+
+    NVGcontext* vg = NULL;
+
     std::cout << "Starting GLFW context, OpenGL 3.3" << std::endl;
     // Init GLFW
     glfwInit();
     // Set all the required options for GLFW
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+    glfwWindowHint(GLFW_STENCIL_BITS, 8);
 
     // Create a GLFWwindow object that we can use for GLFW's functions
     GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
@@ -42,6 +129,9 @@ int main()
         glfwTerminate();
         return -1;
     }
+
+
+
 
     // Set the required callback functions
     glfwSetKeyCallback(window, key_callback);
@@ -55,6 +145,25 @@ int main()
     // Define the viewport dimensions
     glViewport(0, 0, WIDTH, HEIGHT);
 
+
+
+
+
+
+
+    vg = nvgCreateGL3(NVG_STENCIL_STROKES | NVG_ANTIALIAS);
+
+
+
+
+
+
+
+    float pxRatio;
+    double mx, my;
+    int winWidth, winHeight, fbWidth, fbHeight;
+    bool premult = false;
+
     // Game loop
     while (!glfwWindowShouldClose(window))
     {
@@ -66,9 +175,37 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glfwGetCursorPos(window, &mx, &my);
+        glfwGetWindowSize(window, &winWidth, &winHeight);
+        glfwGetFramebufferSize(window, &fbWidth, &fbHeight);
+        // Calculate pixel ration for hi-dpi devices.
+        pxRatio = (float)fbWidth / (float)winWidth;
+
+        // Update and render
+        glViewport(0, 0, fbWidth, fbHeight);
+        if (premult)
+            glClearColor(0,0,0,0);
+        else
+            glClearColor(0.3f, 0.3f, 0.32f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT|GL_STENCIL_BUFFER_BIT);
+
+
+        nvgBeginFrame(vg, winWidth, winHeight, pxRatio);
+
+
+
+            drawGraph(vg, 100, 100, 100, 100, 50);
+
+
+
+        nvgEndFrame(vg);
+
+
         // Swap the screen buffers
         glfwSwapBuffers(window);
     }
+
+    nvgDeleteGL3(vg);
 
     // Terminates GLFW, clearing any resources allocated by GLFW.
     glfwTerminate();
